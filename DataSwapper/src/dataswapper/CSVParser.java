2e6c57f675f5cca36b1d3c6c65fc246c4903f57d
Trump-PC <Trump-PC@Trump-Tower>
2018-09-09 22:25:05 +1000

    Initial

---
+++ DataSwapper/src/dataswapper/CSVParser.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dataswapper;
+
+import java.util.*;
+
+
+/**
+ *
+ * @author Trump-PC
+ */
+public class CSVParser {
+    
+    private ArrayList<String> emailAddress; 
+    
+    public CSVParser(){
+        
+    }
+    
+    public void setDate(String path){
+        
+        
+        
+    }
+    
+    public ArrayList emailAddresss(){
+    
+        return emailAddress;
+    }
+    
+}
---
+++ DataSwapper/src/dataswapper/Engine.java
@@ -0,0 +1,24 @@
+package dataswapper;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Trump-PC
+ */
+public class Engine {
+ 
+    public Engine(){
+        
+    }
+    
+    
+    public void run(){
+        
+    }
+    
+}
