651feaa9634e4f4b864234c0de5a19271bc486a5
Trump-PC <Trump-PC@Trump-Tower>
2018-09-09 22:36:44 +1000

    some comments

--- DataSwapper/src/dataswapper/Engine.java
+++ DataSwapper/src/dataswapper/Engine.java
@@ -1,5 +1,6 @@
 package dataswapper;
 
+import java.util.*;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,12 +13,25 @@ package dataswapper;
  */
 public class Engine {
  
+    
+    private ArrayList<String> emailAddresses;
+    
     public Engine(){
         
     }
     
     
     public void run(){
+        
+        /**
+         * Make an object of the CSV parser, read in email address into emilAddresses arraylist
+         * then use the template engine to sustitue the document id and also the email address 
+         */
+        
+    }
+    
+    
+    public void saveFile(){
         
     }
     
